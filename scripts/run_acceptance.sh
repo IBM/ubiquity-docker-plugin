3649077783b9946ccc31334789f80c42a42b2968
Nagapramod Mandagere <pramod@us.ibm.com>
2017-03-03 14:31:45 -0800

    Updated acceptence tests * takes driver as an argument * fixes some of the test cases (permissions releated)

--- scripts/run_acceptance.sh
+++ scripts/run_acceptance.sh
@@ -1,69 +1,92 @@
 #!/usr/bin/env bash
-echo "First tests"
-echo "Creating volume without  UID:GID"
-VolumeName='acceptanceTestVolume'$(date +%s)
-docker volume create -d spectrum-scale-nfs --name $VolumeName --opt filesystem=gold
 
-echo "Writing to volume without UID:GID should succeed"
-docker run -t -i -d --name acceptanceContainer --volume-driver spectrum-scale-nfs --volume $VolumeName:/data --entrypoint /bin/sh alpine
+if [ -z "$1" ]
+    then
+        DRIVER="spectrum-scale"
+else
+    DRIVER=$1
+fi
+
+
+echo "Starting tests with: "$DRIVER
+echo "********************************************************************"
+echo "Creating volume without UID:GID"
+VolumeName='acceptanceTestVolume'$(date +%s)
+docker volume create -d $DRIVER --name $VolumeName --opt filesystem=gold
+
+echo "1. Writing to volume without UID:GID should succeed"
+docker run -t -i -d --name acceptanceContainer --volume-driver $DRIVER --volume $VolumeName:/data --entrypoint /bin/sh alpine
 docker exec acceptanceContainer touch /data/acceptance.txt
 
-echo "writing to volume with UID:GID should error"
+echo
+echo "2. Writing to volume with UID:GID should succeed"
 docker exec --user 1005:1005 acceptanceContainer  touch /data/unauthorized.txt
 
-echo "Reading from volume without UID:GID should succeed"
+echo
+echo "3. Reading from volume without UID:GID should succeed"
 docker exec acceptanceContainer ls /data
 
-echo "Reading from volume with UID:GUID should error"
+echo
+echo "4. Reading from volume with UID:GUID should succeed"
 docker exec  --user 1005:1005 acceptanceContainer ls /data
 
+echo
 echo "Stoping and removing container and volume"
 docker stop acceptanceContainer
 docker rm acceptanceContainer
 docker volume rm $VolumeName
-
+echo "********************************************************************"
+echo
+echo "********************************************************************"
 echo "Second tests"
 echo "Creating volume with  UID:GID"
 VolumeName='acceptanceTestVolume'$(date +%s)
-docker volume create -d spectrum-scale-nfs --name $VolumeName --opt filesystem=gold --opt uid=1010 --opt gid=1010
+docker volume create -d $DRIVER --name $VolumeName --opt filesystem=gold --opt uid=1010 --opt gid=1010
 
-echo "Writing to volume with UID:GID should succeed"
+echo "1. Writing to volume with UID:GID should succeed"
 
-docker run -t -i -d --name acceptanceContainer --volume-driver spectrum-scale-nfs --volume $VolumeName:/data --user 1010:1010 --entrypoint /bin/sh alpine
+docker run -t -i -d --name acceptanceContainer --volume-driver $DRIVER --volume $VolumeName:/data --user 1010:1010 --entrypoint /bin/sh alpine
 
 docker exec  --user 1010:1010 acceptanceContainer  touch /data/acceptance.txt
 
-echo "Writing to volume with different UID:GID should error"
+echo
+echo "2. Writing to volume with different UID:GID should error"
 docker exec --user 1005:1005 acceptanceContainer   touch /data/unauthorized.txt
 
-echo "Reading from volume should succeed"
+echo
+echo "3. Reading from volume should succeed"
 docker exec acceptanceContainer ls /data
 
-echo "Reading from volume with different UID:GID should error"
+echo
+echo "4. Reading from volume with different UID:GID should error"
 docker exec --user 1005:1005 acceptanceContainer  ls /data
 
-echo "Reading from volume without UID:GID should succeed"
+echo
+echo "5. Reading from volume without UID:GID should succeed"
 docker exec acceptanceContainer ls /data
 
-echo "Reading from volume with UID:GID should succeed"
+echo
+echo "6. Reading from volume with UID:GID should succeed"
 docker exec --user 1010:1010 acceptanceContainer ls /data
 
-echo "Modifying a file created by another user should error"
+echo
+echo "7. Modifying a file created by another user should error"
 docker exec --user 1005:1005 acceptanceContainer sh -c "echo 'unauthorized' >> /data/acceptance.txt"
-
 docker exec --user 1010:1010 acceptanceContainer more /data/acceptance.txt
 docker exec --user 1010:1010 acceptanceContainer sh -c "echo 'unauthorized' >> /data/unauthorized.txt"
 
 docker exec --user 1005:1005 acceptanceContainer more /data/unauthorized.txt
 
-echo "Modifying a file created by same user should succeed"
+echo
+echo "8. Modifying a file created by same user should succeed"
 docker exec --user 1010:1010 acceptanceContainer sh -c "echo 'authorized' >> /data/acceptance.txt"
 
 docker exec --user 1010:1010 acceptanceContainer more /data/acceptance.txt
 
+echo
 echo "Stoping and removing container and volume"
 docker stop acceptanceContainer
 docker rm acceptanceContainer
 docker volume rm $VolumeName
-
+echo "********************************************************************"
 

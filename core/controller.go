4486ec3ff8e20ae3a81f026e5b222bfefbf2b39a
Lior Tamari <liort@il.ibm.com>
2017-09-11 15:11:52 +0300

    Revert "Feature/liort user pass 1 (#47)"
    This reverts commit c965c11d1474a802b7e142c87890e01231665703.

--- core/controller.go
+++ core/controller.go
@@ -70,7 +70,6 @@ func (c *Controller) Create(createVolumeRequest resources.CreateVolumeRequest) r
 		createVolumeRequest.Backend = userSpecifiedBackend.(string)
 	}
 
-	createVolumeRequest.CredentialInfo = c.config.CredentialInfo
 	err := c.client.CreateVolume(createVolumeRequest)
 	var createResponse resources.GenericResponse
 	if err != nil {
@@ -85,7 +84,6 @@ func (c *Controller) Remove(removeVolumeRequest resources.RemoveVolumeRequest) r
 	c.logger.Println("Controller: remove start")
 	defer c.logger.Println("Controller: remove end")
 	// forceDelete is set to false to enable deleting just the volume metadata
-	removeVolumeRequest.CredentialInfo = c.config.CredentialInfo
 	err := c.client.RemoveVolume(removeVolumeRequest)
 	if err != nil {
 		return resources.GenericResponse{Err: err.Error()}
@@ -97,7 +95,6 @@ func (c *Controller) Mount(attachRequest resources.AttachRequest) resources.Atta
 	c.logger.Println("Controller: mount start")
 	defer c.logger.Println("Controller: mount end")
 
-	attachRequest.CredentialInfo = c.config.CredentialInfo
 	mountedPath, err := c.client.Attach(attachRequest)
 	if err != nil {
 		return resources.AttachResponse{Err: err.Error()}
@@ -111,7 +108,6 @@ func (c *Controller) Unmount(detachRequest resources.DetachRequest) resources.Ge
 	c.logger.Println("Controller: unmount start")
 	defer c.logger.Println("Controller: unmount end")
 
-	detachRequest.CredentialInfo = c.config.CredentialInfo
 	err := c.client.Detach(detachRequest)
 	if err != nil {
 		return resources.GenericResponse{Err: err.Error()}
@@ -123,7 +119,6 @@ func (c *Controller) Unmount(detachRequest resources.DetachRequest) resources.Ge
 func (c *Controller) Path(pathRequest resources.GetVolumeConfigRequest) resources.AttachResponse {
 	c.logger.Println("Controller: path start")
 	defer c.logger.Println("Controller: path end")
-	pathRequest.CredentialInfo = c.config.CredentialInfo
 	volume, err := c.client.GetVolumeConfig(pathRequest)
 	if err != nil {
 		return resources.AttachResponse{Err: err.Error()}
@@ -140,7 +135,6 @@ func (c *Controller) Path(pathRequest resources.GetVolumeConfigRequest) resource
 func (c *Controller) Get(getRequest resources.GetVolumeConfigRequest) resources.DockerGetResponse {
 	c.logger.Println("Controller: get start")
 	defer c.logger.Println("Controller: get end")
-	getRequest.CredentialInfo = c.config.CredentialInfo
 	volStatus, err := c.client.GetVolumeConfig(getRequest)
 	if err != nil {
 		return resources.DockerGetResponse{Err: err.Error()}
@@ -160,7 +154,7 @@ func (c *Controller) Get(getRequest resources.GetVolumeConfigRequest) resources.
 func (c *Controller) List() resources.ListResponse {
 	c.logger.Println("Controller: list start")
 	defer c.logger.Println("Controller: list end")
-	listVolumesRequest := resources.ListVolumesRequest{Backends: c.config.Backends, CredentialInfo:c.config.CredentialInfo}
+	listVolumesRequest := resources.ListVolumesRequest{Backends: c.config.Backends}
 	volumes, err := c.client.ListVolumes(listVolumesRequest)
 	if err != nil {
 		return resources.ListResponse{Err: err.Error()}

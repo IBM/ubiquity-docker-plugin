b75c385b0a7c668d2d151c276f14268d9e1f2f92
Amit Warke <aswarke@us.ibm.com>
2017-10-05 13:25:54 -0700

    Adding tag to Plugin v2 container

--- scripts/plugin_v2.sh
+++ scripts/plugin_v2.sh
@@ -3,24 +3,25 @@
 set -xe
 
 PLUGIN_NAME="aswarke/ubiquity-docker-plugin"
+TAG="v1"
 scripts=$(dirname $0)
 PLUGIN_V2_DIR=$scripts/../pluginv2
 
-echo "Creating docker plugin $PLUGIN_NAME"
+echo "Creating docker plugin $PLUGIN_NAME:$TAG"
 
-docker build -f Dockerfile -t $PLUGIN_NAME $scripts/..
+docker build -f Dockerfile -t $PLUGIN_NAME:$TAG $scripts/..
 
-id=$(docker create $PLUGIN_NAME true)
+id=$(docker create $PLUGIN_NAME:$TAG true)
 
 rm -rf $PLUGIN_V2_DIR
 mkdir -p $PLUGIN_V2_DIR/rootfs
 
 docker export $id | tar -x -C $PLUGIN_V2_DIR/rootfs
 docker rm -vf $id
-docker rmi $PLUGIN_NAME
+docker rmi $PLUGIN_NAME:$TAG
 
 cp config.json $PLUGIN_V2_DIR
 
-docker plugin create $PLUGIN_NAME $PLUGIN_V2_DIR
+docker plugin create $PLUGIN_NAME:$TAG $PLUGIN_V2_DIR
 
-echo "docker plugin $PLUGIN_NAME successfully created"
+echo "docker plugin $PLUGIN_NAME:$TAG successfully created"
